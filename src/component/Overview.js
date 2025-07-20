c06f30b74d132a37613cbe566ca20bb190c9afc3
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-20 05:46:17 +0300

    Declare variables in Overview.js of total saved and completed Goals

--- src/component/Overview.js
+++ src/component/Overview.js
@@ -1,4 +1,7 @@
-export default function Overview() {
+export default function Overview({ goals }) {
+
+    const totalSaved = goals.reduce((sum, goal) => sum + goal.savedAmount, 0)
+    const completedGoals = goals.filter(goal => goal.savedAmount >= goal.targetAmount).length
     
     return (
         <>
