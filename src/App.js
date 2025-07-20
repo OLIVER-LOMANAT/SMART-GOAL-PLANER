1e064e3e2c65ad1c256493397293250ba2e0de15
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-20 05:08:52 +0300

    Adds useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App() {
+  //Setting goals as empty array 
   const [goal, setGoals] = useState([]);
   const [currentGoal, setCurrentGoal] = useState({
     name: "",
@@ -8,9 +9,22 @@ export default function App() {
     category: "Travel",
     deadline: "",
   });
+  //Deposit Data
   const [depositData, setDepositData] = useState({
-    
+    goalId: "",
+    amount: ""
   })
+
+  useEffect(() => {
+    fetch("https://localhost:3000/goals")
+    .then(res => res.json())
+    .then(data => setGoals(data))
+  }, []);
+
+  function handleGoalChange() {
+    
+  }
+
   return (
    
   )
